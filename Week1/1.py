f91be18a1a1e60ea7b01c1508224f0da760e2e90
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-09-07 01:59:01 -0400

    Update 1.py

--- Week1/1.py
+++ Week1/1.py
@@ -1,34 +1,79 @@
-import requests
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 from bs4 import BeautifulSoup
+import pandas as pd
+import requests
 import re
 
-def get_soup(url):
-    html = requests.get(url)
-    txt = html.text
-    soup = BeautifulSoup(txt, 'lxml')
-    return soup
+def combine_url(url1, url2):
+    return '%s%s' % (url1, url2)
 
-def get_link(url):
-    soup = get_soup(url)
-    text = soup.findAll('table')[0].findAll('td')
-    rows = []
-    for x in text:
-        rows.append(str(x))
-    valid = []
-    for row in rows:
-        pattern = '<a href=.+</a>'
-        pattern1 = '".+"'
-        result0 = re.search(pattern, row)
-        result = re.search(pattern1, result0)
-        if result:
-            link = result.group(0)
-            link = link.strip('"')
-            valid.append(link)
-    return valid
-            
-url = 'https://storm.cis.fordham.edu/~yli/data/LoveOutput/'
-valid = get_link(url)
-print(valid)
-with open('/Users/lordxuzhiyu/Desktop/valid_ip.txt', 'a') as f:
-    for item in valid:
-        f.write("%s\n" % item)
+def get_text(url, head):
+    html = requests.get(url, headers = head)
+    txt = html.text
+    return txt
+
+head = {
+    'User-Agent':'Mozilla/4.0(compatible;MSIE 5.5;Windows NT)',
+}
+
+url0 = 'https://storm.cis.fordham.edu/~yli/data/LoveOutput/'
+#soup = BeautifulSoup(txt,'lxml')
+
+table0 = pd.DataFrame(columns = ['Author', 'Title', 'Tags', 'Body', 'Link'])
+#success = 0
+fails = 0
+
+for line in open('/Users/lordxuzhiyu/Desktop/valid_ip.txt'):
+    url = combine_url(url0, line)
+    url = url.replace(' ', '').replace('\n', '').replace('\t', '').replace('\r', '').strip()
+    try:
+        #p = requests.get(url, headers = head, timeout = 5)
+        #success += 1
+        html = requests.get(url,headers=head)
+        txt = html.text
+        #print(html)
+        text = txt.splitlines()
+        # Title
+        title = re.findall(r'(\w+) By', text[0]) 
+        # Author
+        author = re.findall(r'By (.+)', text[0])
+        # Tags
+        tag = text[2].split(' ;')
+        tag = ', '.join(tag)
+        # Body
+        body = text[4].replace('<br><br>', '[P]')
+        body = body.replace('<br>', '[L]')
+        #print(body)
+        # Link
+        link = re.findall(r'http.+', text[6])        
+        record = {'Author': author[0], 'Title': title, 'Tags': tag, 'Body': body, 'Link': link[0]}
+        temp = pd.DataFrame(record, index = [0])
+        #print(record)
+        table0 = table0.append(temp, ignore_index = True)
+        print('Suceess')
+    
+    except Exception as error:
+        fails += 1
+        print(error)
+        continue
+
+print('Fails: ', fails)
+#print(table0)
+# sum of poems
+print('sum of poems', len(table0.index))
+
+# sum of different authors
+sum_author = len(table0['Author'].unique().tolist())
+print('sum of different authors', sum_author)
+ 
+# author sorted
+print(table0['Author'].value_counts())
+       
+table0.to_csv('/Users/lordxuzhiyu/Desktop/CleanOutputLoveOutput.csv', 
+              index = None, header = True)
