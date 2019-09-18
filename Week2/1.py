69660742f5f7da438250cadb94e3328815c6dabf
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-09-17 21:31:50 -0400

    Update 1.py

--- Week2/1.py
+++ Week2/1.py
@@ -7,26 +7,44 @@ Created on Sun Sep 15 23:48:19 2019
 """
 
 import nltk
-import pandas as pd
+import re
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 
 class NGram:
     def _init_(self, n):
         self.n = n
         
     def update(self, text):
-        return 0
+        text = text.lower()
+        text = re.sub(r'[^a-zA-Z0-9\s]', ' ', text)
+        tokens = [token for token in text.split(" ") if token != ""]
+        ngrams = zip(*[tokens[i:] for i in range(self.n)])
+        return [" ".join(ngram) for ngram in ngrams]
     
     def get_vocab(self):
-        return 0
+        tokens = nltk.word_tokenize(self)
+        return set(tokens)
     
     def size_vocab(self):
-        return 0
+        size = len(self.get_vocab())
+        return size
     
     def prob(self, context, word):
-        return 0
+        tokens = word_tokenize(context)
+        fdist = FreqDist(tokens)
+        if len(tokens)> 1000:
+            probability = 1 / len(tokens)
+        elif word in fdist:
+            probability = float(fdist[word]) / len(tokens)
+        else:
+            probability = 1 / (len(tokens) + 1)
+        return probability
     
     def len_text(self):
-        return 0
+        tokenized_word = word_tokenize(self)
+        return len(tokenized_word)
     
     def len_ngram(self):
         return 0
