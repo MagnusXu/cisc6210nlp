f09a281a6feb4c7062e374d5b659204a817fbf4d
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-09-26 01:59:44 -0400

    Update 0.py

--- Week 3/0.py
+++ Week 3/0.py
@@ -1,1 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import random, re
 
+head = {
+    'User-Agent':'Mozilla/4.0(compatible;MSIE 5.5;Windows NT)',
+}
+
+positive_url = 'https://storm.cis.fordham.edu/~yli/data/electronics/positive.review'
+negative_url = 'https://storm.cis.fordham.edu/~yli/data/electronics/negative.review'
+
+good_html = requests.get(positive_url, headers = head)
+bad_html = requests.get(negative_url, headers = head)
+good_review = good_html.text  
+bad_review = bad_html.text  
+
+soup = bs(good_review, 'lxml')
+text = soup.find_all('review_text')
+s = [l.text for l in text]
+
+
+
+print(' ', s)
